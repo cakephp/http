0a51b7d97f9ec302fa5bf329a41dfa5ff4a1659a
Ian den Hartog <iandenhartog@hotmail.com>
2018-04-24 11:48:46 +0200

    Update the range of isOk as discussed in #11796

--- Client/Response.php
+++ Client/Response.php
@@ -233,15 +233,7 @@ class Response extends Message implements ResponseInterface
      */
     public function isOk()
     {
-        $codes = [
-            static::STATUS_OK,
-            static::STATUS_CREATED,
-            static::STATUS_ACCEPTED,
-            static::STATUS_NON_AUTHORITATIVE_INFORMATION,
-            static::STATUS_NO_CONTENT
-        ];
-
-        return in_array($this->code, $codes);
+        return $this->code >= 200 && $this->code <= 399;
     }
 
     /**
@@ -272,6 +264,11 @@ class Response extends Message implements ResponseInterface
      */
     public function statusCode()
     {
+        deprecationWarning(
+            'Response::statusCode() is deprecated. ' .
+            'Use Response::getStatusCode() instead.'
+        );
+
         return $this->code;
     }
 
@@ -319,6 +316,11 @@ class Response extends Message implements ResponseInterface
      */
     public function encoding()
     {
+        deprecationWarning(
+            'Response::encoding() is deprecated. ' .
+            'Use Response::getEncoding() instead.'
+        );
+
         return $this->getEncoding();
     }
 
@@ -354,6 +356,11 @@ class Response extends Message implements ResponseInterface
      */
     public function header($name = null)
     {
+        deprecationWarning(
+            'Response::header() is deprecated. ' .
+            'Use Response::getHeader() and getHeaderLine() instead.'
+        );
+
         if ($name === null) {
             return $this->_getHeaders();
         }
@@ -381,6 +388,11 @@ class Response extends Message implements ResponseInterface
      */
     public function cookie($name = null, $all = false)
     {
+        deprecationWarning(
+            'Response::cookie() is deprecated. ' .
+            'Use Response::getCookie(), getCookieData() or getCookies() instead.'
+        );
+
         if ($name === null) {
             return $this->getCookies();
         }
@@ -420,7 +432,7 @@ class Response extends Message implements ResponseInterface
      * Get the value of a single cookie.
      *
      * @param string $name The name of the cookie value.
-     * @return string|null Either the cookie's value or null when the cookie is undefined.
+     * @return string|array|null Either the cookie's value or null when the cookie is undefined.
      */
     public function getCookie($name)
     {
@@ -511,6 +523,11 @@ class Response extends Message implements ResponseInterface
      */
     public function version()
     {
+        deprecationWarning(
+            'Response::version() is deprecated. ' .
+            'Use Response::getProtocolVersion() instead.'
+        );
+
         return $this->protocol;
     }
 
