ddf39585ee026a30950ad810f0d516f351075fee
mscherer <dereuromark@web.de>
2024-11-20 00:13:19 +0100

    Merge 5.x into 5.next

--- Response.php
+++ Response.php
@@ -436,7 +436,7 @@ class Response implements ResponseInterface, Stringable
      * This is needed for RequestHandlerComponent and recognition of types.
      *
      * @param string $type Content type.
-     * @param list<string>|string $mimeType Definition of the mime type.
+     * @param array<string>|string $mimeType Definition of the mime type.
      * @return void
      */
     public function setTypeMap(string $type, array|string $mimeType): void
@@ -779,7 +779,7 @@ class Response implements ResponseInterface, Stringable
      * separated string. If no parameters are passed, then an
      * array with the current Vary header value is returned
      *
-     * @param list<string>|string $cacheVariances A single Vary string or an array
+     * @param array<string>|string $cacheVariances A single Vary string or an array
      *   containing the list for variances.
      * @return static
      */
